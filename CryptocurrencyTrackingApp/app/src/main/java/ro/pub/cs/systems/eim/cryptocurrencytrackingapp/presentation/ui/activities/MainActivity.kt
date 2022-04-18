50711d50ea639976fb7704f1b206ae30321cb4e2
Iuri-Mihai Meresan <iuri_mihai.meresan@stud.acs.upb.ro>
2022-04-18 07:57:18 +0300

    Coroutines check

--- CryptocurrencyTrackingApp/app/src/main/java/ro/pub/cs/systems/eim/cryptocurrencytrackingapp/presentation/ui/activities/MainActivity.kt
+++ CryptocurrencyTrackingApp/app/src/main/java/ro/pub/cs/systems/eim/cryptocurrencytrackingapp/presentation/ui/activities/MainActivity.kt
@@ -2,8 +2,8 @@ package ro.pub.cs.systems.eim.cryptocurrencytrackingapp.presentation.ui.activiti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
+import androidx.lifecycle.whenStarted
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
@@ -11,7 +11,9 @@ import kotlinx.coroutines.launch
 import ro.pub.cs.systems.eim.cryptocurrencytrackingapp.R
 import ro.pub.cs.systems.eim.cryptocurrencytrackingapp.data.repository.CoinRepository
 import ro.pub.cs.systems.eim.cryptocurrencytrackingapp.domain.models.Coin
+import ro.pub.cs.systems.eim.cryptocurrencytrackingapp.domain.use_cases.GetCoinsListUseCase
 import ro.pub.cs.systems.eim.cryptocurrencytrackingapp.domain.use_cases.UpdatePriceUseCase
+import ro.pub.cs.systems.eim.cryptocurrencytrackingapp.presentation.view_models.CoinsListViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +21,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.tvId)
+        val textView2 = findViewById<TextView>(R.id.tvId2)
         val coin = Coin("eth-ethereum", "Ethereum", "ETH")
+        val coin2 = Coin("btc-bitcoin", "Bitcoin", "BTC")
 
         val priceUS = UpdatePriceUseCase(CoinRepository)
+        val coinsUS = GetCoinsListUseCase(CoinRepository)
 
-        CoroutineScope(Dispatchers.Main).launch {
-            val price = priceUS(coin.symbol, "USDT")
-            price.collect {
-//                textView.text = it.toString()
-                Log.d("Coin price", it)
+//        CoroutineScope(Dispatchers.Main).launch {
+//            val price = priceUS(coin.symbol, "USDT")
+//            price.collect {
+//                textView.text = it
+//                Log.d("Coin price", it)
+//            }
+//        }
+
+        val vm = CoinsListViewModel(coinsUS, priceUS)
+
+//        val
+        CoroutineScope(Dispatchers.Default).launch {
+            lifecycle.whenStarted {
+                val coinPrice = vm.getUpdatedPrice(coin, "USDT")
+                coinPrice.collect { textView.text = it }
             }
         }
+
+        CoroutineScope(Dispatchers.Default).launch {
+            lifecycle.whenStarted {
+                val coinPrice = vm.getUpdatedPrice(coin2, "USDT")
+                coinPrice.collect { textView2.text = it }
+            }
+        }
+
+//        vm.fetchCoinPrice(coin, "USDT")
+
+//        vm.getCoinPriceUpdate(coin, "USDT").observe()
+
+//        CoroutineScope(Dispatchers.IO).launch { )
+//            Log.d("Get all coins", coinsUS().toString())
+//        }
+
+//        vm.fetchCoinPrice(vm.coins.value!![1], "USDT", textView)
     }
 }
