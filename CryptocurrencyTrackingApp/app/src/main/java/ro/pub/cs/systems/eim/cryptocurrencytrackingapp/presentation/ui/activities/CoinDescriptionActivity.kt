1d8e75c0ecd53db4b8840ee17b69e0a948fe3a08
Iuri-Mihai Meresan <iuri_mihai.meresan@stud.acs.upb.ro>
2022-04-18 07:55:46 +0300

    Some tests

---
+++ CryptocurrencyTrackingApp/app/src/main/java/ro/pub/cs/systems/eim/cryptocurrencytrackingapp/presentation/ui/activities/CoinDescriptionActivity.kt
@@ -0,0 +1,6 @@
+package ro.pub.cs.systems.eim.cryptocurrencytrackingapp.presentation.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CoinDescriptionActivity: AppCompatActivity() {
+}
